e5db4eaec98a464597b31922a251a18f2058c152
Sparsh <sparsh.raj30@gmail.com>
2025-12-13 12:55:10 +0530

    Updated configuration
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- cartiq-app/src/main/java/com/cartiq/app/CartiqApplication.java
+++ cartiq-app/src/main/java/com/cartiq/app/CartiqApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = "com.cartiq")
+@SpringBootApplication(
+        scanBasePackages = "com.cartiq",
+        exclude = {UserDetailsServiceAutoConfiguration.class}
+)
 @EntityScan(basePackages = "com.cartiq")
 @EnableJpaRepositories(basePackages = "com.cartiq")
 @EnableJpaAuditing
