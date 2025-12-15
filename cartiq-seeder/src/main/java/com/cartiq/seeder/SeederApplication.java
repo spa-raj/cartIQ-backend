78181c93b2ebd3a98cdbbdf6e4f2dec0da5e16be
Sparsh <sparsh.raj30@gmail.com>
2025-12-15 22:05:40 +0530

    Fix cartiq-seeder build for Spring Boot 4.0 compatibility
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- cartiq-seeder/src/main/java/com/cartiq/seeder/SeederApplication.java
+++ cartiq-seeder/src/main/java/com/cartiq/seeder/SeederApplication.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.nio.file.Path;
